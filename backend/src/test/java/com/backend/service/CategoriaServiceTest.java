ce03b7e6653444afcc18b7b41f7048d1c8004faa
Damian Micheli <damianmicheli@gmail.com>
2022-10-21 11:38:00 -0300

    application.properties - cambiar el modo de hibernate de create a auto para que no se borre la tabla en cada ejecucion

--- backend/src/test/java/com/backend/service/CategoriaServiceTest.java
+++ backend/src/test/java/com/backend/service/CategoriaServiceTest.java
@@ -20,7 +20,7 @@ class CategoriaServiceTest {
 
     public CategoriaDTO crearNuevaCategoria() {
         CategoriaDTO categoria = new CategoriaDTO();
-        categoria.setTitulo("Casa1");
+        categoria.setTitulo("Casa");
         categoria.setDescripcion("Casa completa");
         categoria.setUrl("https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80");
 
