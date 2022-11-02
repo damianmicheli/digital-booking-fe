29c0afdda272636344c99abb0687b9fff856bd77
Guadalupe Montero <guadamontero10@gmail.com>
2022-11-02 20:47:30 -0300

    arreglando responsive categorias

--- frontend/src/components/categorias/CardsCategorias.jsx
+++ frontend/src/components/categorias/CardsCategorias.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import CardCategoriaItem from './CardCategoriaItem';
-import styles from './cardsCategorias.module.css';
+import React from "react";
+import CardCategoriaItem from "./CardCategoriaItem";
+import styles from "./cardsCategorias.module.css";
 
 const CardsCategorias = ({ items }) => {
   return (
     <div className={styles.container}>
-      <h2>Buscar por tipo de alojamiento</h2>
+        <h2>Buscar por tipo de alojamiento</h2>
       <div className={styles.containerCategory}>
-        {items.map(alojamiento => 
-                <CardCategoriaItem 
-                    category={alojamiento.category}
-                    img={alojamiento.img}
-                />
-        )}
+        {items.map((alojamiento) => (
+          <CardCategoriaItem
+            category={alojamiento.category}
+            img={alojamiento.img}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CardsCategorias;
