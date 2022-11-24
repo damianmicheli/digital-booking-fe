7b3601a331868cf675ec47162895b99a6b8e72af
Guadalupe Montero <guadamontero10@gmail.com>
2022-11-23 23:19:57 -0300

    Actualizo en el componente Home el endpoint de categorias

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router";
 
 import Search from "../search/Search";
 import CardsCategories from "../categories/CardsCategories";
@@ -13,8 +14,10 @@ import { FavProvider } from "../../context/FavContext";
 import URL_BASE from "../global/getUrlBase";
 
 const Home = () => {
+
+  const { id } = useParams();
   
-  const [categorias] = useFetch(`${URL_BASE}/categorias`);
+  const [categorias] = useFetch(`${URL_BASE}/categorias?/buscar?id=${id}`);
 
   return (
     <>
