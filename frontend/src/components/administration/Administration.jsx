622a2aeb1bbb7f86afed8590b73c4f4f8c2b6d21
Marina Victoria Blanco <marivic_05@hotmail.com>
2022-12-01 17:33:24 -0300

    Avances en form alta de producto

--- frontend/src/components/administration/Administration.jsx
+++ frontend/src/components/administration/Administration.jsx
@@ -3,15 +3,16 @@ import styles from "./administration.module.css";
 
 import { useParams } from "react-router";
 
-import useMediaQuery from "../../hooks/useMediaQuery";
-
 import HeaderProduct from "../product/content/HeaderProduct";
+import Icon from "../global/Icon";
+import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 
 import useFetch from "../../hooks/useFetch";
 import AuthContext from "../../context/AuthContext";
 import URL_BASE from "../global/getUrlBase";
 
 import SuccessAdmin from "../global/modal/success/SuccessAdmin";
+import Button from "../global/Button";
 
 const Administration = () => {
   const { userLog } = useContext(AuthContext);
@@ -23,7 +24,7 @@ const Administration = () => {
 
   const [categorias] = useFetch(`${URL_BASE}/categorias`);
   const [ciudades] = useFetch(`${URL_BASE}/ciudades`);
-  const [atributos] = useFetch(`${URL_BASE}/ciudades`);
+  const [atributos] = useFetch(`${URL_BASE}/caracteristicas`);
 
   const [success, setSuccess] = useState(false);
 
@@ -47,67 +48,95 @@ const Administration = () => {
               <h2 className="heading2 color2 paddingTop">Crear propiedad</h2>
             </div>
             <div className={styles.content}>
-              <div className={styles.groupForm}>
-                <label className="text2">Nombre de la propiedad</label>
-                <input type="text" id="name" name="name" />
-              </div>
-              <div className={styles.groupForm}>
-                <label className="text2">Categoría</label>
-                <select name="category">
-                  <option selected="selected">Elegí una categoría</option>
-                  {categorias &&
-                    categorias.map((categoria) => (
-                      <option key={categoria.id} value={categoria.titulo}>
-                        {categoria.titulo}
-                      </option>
-                    ))}
-                </select>
-              </div>
-              <div className={styles.groupForm}>
-                <label className="text2">Dirección</label>
-                <input type="text" id="address" name="address" />
-              </div>
-              <div className={styles.groupForm}>
-                <label className="text2">Ciudad</label>
-                <select name="city">
-                  <option selected="selected">Elegí una ciudad</option>
-                  {ciudades &&
-                    ciudades.map((ciudad) => (
-                      <option key={ciudad.id} value={ciudad.ciudad}>
-                        {ciudad.ciudad}, {ciudad.pais}
-                      </option>
-                    ))}
-                </select>
+              <div className={styles.contentFlex}>
+                <div className={styles.groupForm}>
+                  <label className="text2">Nombre de la propiedad</label>
+                  <input type="text" id="name" name="name" />
+                </div>
+                <div className={styles.groupForm}>
+                  <label className="text2">Categoría</label>
+                  <select name="category">
+                    <option selected="selected">Elegí una categoría</option>
+                    {categorias &&
+                      categorias.map((categoria) => (
+                        <option key={categoria.id} value={categoria.titulo}>
+                          {categoria.titulo}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                <div className={styles.groupForm}>
+                  <label className="text2">Dirección</label>
+                  <input type="text" id="address" name="address" />
+                </div>
+                <div className={styles.groupForm}>
+                  <label className="text2">Ciudad</label>
+                  <select name="city">
+                    <option selected="selected">Elegí una ciudad</option>
+                    {ciudades &&
+                      ciudades.map((ciudad) => (
+                        <option key={ciudad.id} value={ciudad.ciudad}>
+                          {ciudad.ciudad}, {ciudad.pais}
+                        </option>
+                      ))}
+                  </select>
+                </div>
               </div>
               <div className={styles.groupForm}>
                 <label className="text2">Descripción</label>
                 <textarea id="description" name="description" />
               </div>
               <h2 className="heading2 color2 paddingTop">Atributos</h2>
-              <div>
-              <div className={styles.groupForm}>
-                <label className="text2">Nombre</label>
-                <select name="city">
-                  <option selected="selected">Elegí un atributo</option>
-                  {atributos &&
-                    atributos.map((atributo) => (
-                      <option key={atributo.id} value={atributo}>                      
-                      </option>
-                    ))}
-                </select>
+              <div className={styles.contentAtributes}>
+                <div className={styles.groupForm}>
+                  <label className="text2">Nombre e ícono</label>
+                  <select name="city">
+                    <option selected="selected">Elegí un atributo</option>
+                    {atributos &&
+                      atributos.map((atributo) => (
+                        <option key={atributo.id} value={atributo.nombre}>
+                          {" "}
+                          {atributo.nombre}{" "}
+                          <Icon css={styles.icon} icon={atributo.icono} />
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                <Icon css={styles.addIcon} icon={faSquarePlus} />
               </div>
-              <div className={styles.groupForm}>
-                <label className="text2">Ícono</label>
-                <select name="city">
-                  <option selected="selected">(icono)</option>
-                  {atributos &&
-                    atributos.map((atributo) => (
-                      <option key={atributo.id} value={atributo}>                      
-                      </option>
-                    ))}
-                </select>
+              <h2 className="heading2 color2 paddingTop">
+                Políticas del producto
+              </h2>
+              <div className={styles.contentPolitics}>
+                <div className={styles.groupForm}>
+                  <h4 className="heading4">Normas de la casa</h4>
+                  <label className="text2">Descripción</label>
+                  <textarea id="description" name="description" />
+                </div>
+                <div className={styles.groupForm}>
+                  <h4 className="heading4">Salud y seguridad</h4>
+                  <label className="text2">Descripción</label>
+                  <textarea id="description" name="description" />
+                </div>
+                <div className={styles.groupForm}>
+                  <h4 className="heading4">Políticas de cancelación</h4>
+                  <label className="text2">Descripción</label>
+                  <textarea id="description" name="description" />
+                </div>
               </div>
+              <h2 className="heading2 color2 paddingTop">Cargar imágenes</h2>
+              <div className={styles.contentImages}>
+                <div className={styles.groupForm}>
+                  <input
+                    type="text"
+                    id="urlImage"
+                    name="urlImage"
+                    placeholder="Insertar https://"
+                  />
+                </div>
+                <Icon css={styles.addIcon} icon={faSquarePlus} />
               </div>
+              <Button text="Crear" css="button4 centered" />
             </div>
           </form>
         </div>
