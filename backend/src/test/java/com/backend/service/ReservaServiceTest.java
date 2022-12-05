78fcb6fea4d68e7a264d6b77baf66db1ca255018
Damian Micheli <damianmicheli@gmail.com>
2022-12-05 00:12:09 -0300

    Restringir pipeline a main

--- backend/src/test/java/com/backend/service/ReservaServiceTest.java
+++ backend/src/test/java/com/backend/service/ReservaServiceTest.java
@@ -1,9 +1,11 @@
 package com.backend.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ReservaServiceTest {
 
     @Test
