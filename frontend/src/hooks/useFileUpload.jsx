202a1af847b57117b174843eceb7b085012a0f29
Marina Victoria Blanco <marivic_05@hotmail.com>
2022-12-13 13:21:20 -0300

    Carga múltiples imágenes pero devuelve sólo 1 url de AWS

--- frontend/src/hooks/useFileUpload.jsx
+++ frontend/src/hooks/useFileUpload.jsx
@@ -3,26 +3,68 @@ import { useState } from "react";
 import URL_BASE from "../components/global/getUrlBase";
 
 const useFileUpload = () => {
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-  const [urlImageAws, setUrlImageAws] = useState("");
-  const [uploadSuccess, setUploadSuccess] = useState(null);
+  // const [selectedFile, setSelectedFile] = useState();
+  // const [isFilePicked, setIsFilePicked] = useState(false);
+  const [urlImagesAws, setUrlImagesAws] = useState([]);
+  // const [uploadSuccess, setUploadSuccess] = useState(null);
+  const [files, setFiles] = useState("");
+  const [fileUploadProgress, setFileUploadProgress] = useState(false);
+  //for displaying response message
+  const [fileUploadResponse, setFileUploadResponse] = useState(null);
 
   const changeHandler = (e) => {
-    setSelectedFile(e.target.files[0]);
-    setIsFilePicked(true);
+    setFiles(e.target.files);
   };
 
-  console.log({ selectedFile, isFilePicked, uploadSuccess });
+  console.log(files);
+
+  const formData = new FormData();
+
+  const uploadImage = async (i) => {
+    const response = await fetch(`${URL_BASE}/s3/subir`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      alert("No se pudo generar la url de aws");
+    } 
+    const data = await response.json();
+    if (data) {
+      setUrlImagesAws([...urlImagesAws, data]);
+      console.log("se cargó imagen " + i);
+      //setUploadSuccess("Carga de imagen finalizada");
+    }
+  };
 
   const handleSubmission = () => {
     console.log("Se disparó el submission");
-    const formData = new FormData();
-    formData.append("file", selectedFile);
 
-    setUploadSuccess("Cargando imagen...");
+    setFileUploadProgress(true);
+    setFileUploadResponse(null);
 
-    fetch(`${URL_BASE}/s3/subir`, {
+    //setUploadSuccess("Cargando imagen...");
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append("file", files[i]);
+      uploadImage(i);
+    }
+  };
+
+  return {
+    files,
+    fileUploadProgress,
+    fileUploadResponse,
+    urlImagesAws,
+    handleSubmission,
+    changeHandler,
+  };
+};
+
+export default useFileUpload;
+
+/* const uploadImage = async (i) => {
+    await fetch(`${URL_BASE}/s3/subir`, {
       method: "POST",
       body: formData,
     })
@@ -36,23 +78,12 @@ const useFileUpload = () => {
       .then((data) => {
         console.log(data);
         if (data) {
-          setUrlImageAws(data);
-          setUploadSuccess("Carga de imagen finalizada");
+          setUrlImagesAws([...urlImagesAws, data]);
+          console.log("se cargó imagen " + i);
+          //setUploadSuccess("Carga de imagen finalizada");
         }
       })
       .catch((error) => {
         console.error("Error:", error);
       });
-  };
-
-  return {
-    selectedFile,
-    isFilePicked,
-    urlImageAws,
-    uploadSuccess,
-    handleSubmission,
-    changeHandler,
-  };
-};
-
-export default useFileUpload;
+  }; */
